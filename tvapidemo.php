ed4207f144404ef12e662623b55fe059a92423ee
Joseph Tinsley <joseph.tinsley@gmail.com>
2016-04-09 21:58:35 -0400

    Show Runtimes

--- tvapidemo.php
+++ tvapidemo.php
@@ -8,6 +8,12 @@ $url  = 'http://api.tvmaze.com/schedule?country=US&date=2015-04-01';
 $json = file_get_contents($url);
 $retVal = json_decode($json, TRUE);
 
-for ($x = 0; $x < count($retVal); $x++) {
+for ($x = 0; $x < count($retVal); $x++) 
+{
+
+	// DISPLAYS SHOW NAMES
     echo $retVal[$x]['show']['name']."<br/>";
+    
+    //DISPLAYS SHOW RUNTIMES
+    echo $retVal[$x]['show']['runtime']."<br/>";
 }
